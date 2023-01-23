f6e639a4d72f44f618b6adde0a047adb45f7b70a
Agostinho Neto <agostneto6@gmail.com>
2023-01-23 18:58:54 -0300

    criação de auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB as FacadesDB;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $users = DB::table('teste')->get();
+    dd($users);
     return view('welcome');
-    echo "oiiiiidfdfsddfs";
+    
 });
