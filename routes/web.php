824e910c82cfaf4257bfc57c28ed06f99ef23c25
Agostinho Neto <agostneto6@gmail.com>
2023-01-29 15:42:43 -0300

    criação do services User crud - hml

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+//
 Route::controller(AuthController::class)->group(function () {
     Route::get('register', 'register');
 });
