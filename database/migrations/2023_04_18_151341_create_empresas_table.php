800612a8cb0bb02b089547dff485b29d9bb6354f
Agostinho Neto <agostneto6@gmail.com>
2023-04-18 18:03:16 -0300

    Criando migration empresas

---
+++ database/migrations/2023_04_18_151341_create_empresas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa', function (Blueprint $table) {
+            $table->integer('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('nome');
+            $table->string('endereco');
+            $table->string('telefone');
+            $table->string('email');
+            $table->timestamps();          
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
