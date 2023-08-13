b32b7e4c3ef7cbacbaf46b39d2d183ee0fe9f57f
agostinho f s neto silva netto <agostneto6@gmail.com>
2023-08-13 11:52:23 -0300

    refatorando delete empresas e filial

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -19,42 +19,6 @@ class LoginController extends Controller
        }
 
        return new UserResource(auth()->user());
-
-        /*
-        $credentials = $request->only('email', 'password');
-        //valid credential
-        $validator = Validator::make($credentials, [
-            'email' => 'required|email',
-            'password' => 'required|string|min:6|max:50'
-        ]);
-
-        //Send failed response if request is not valid
-        if ($validator->fails()) 
-        {
-            return response()->json(['error' => $validator->messages()],
-            Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-        //Request is validated
-        //Crean token
-        try {
-            if (!$token = FacadesJWTAuth::attempt($credentials)) {
-                return response()->json([
-                	'success' => false,
-                	'message' => 'Credenciais Invalidas.',
-                ], 400);
-            }
-        } catch (ExceptionsJWTException $e) {
-            return response()->json([
-              	'success' => false,
-               	'message' => 'não foi possível criar Token.',
-           ], );
-        }
- 	   //Token created, return with success response and jwt token
-        return response()->json([
-            'success' => true,
-            'token' => $token,
-        ]);
-          */
     }     
 }
 
